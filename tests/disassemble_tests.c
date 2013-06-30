c1de4febcf110c57f0d3ed4126d8cf9d7f6607d9
Matt Boyer <mboyer@sdf.org>
2013-06-30 15:25:47 +0100

    Imported assert macros for pointers

--- tests/disassemble_tests.c
+++ tests/disassemble_tests.c
@@ -3,6 +3,15 @@
 #include "../disassemble.c"
 
 #define ck_assert_int_gt(X, Y) _ck_assert_int(X, >, Y)
+/* Pointer comparsion macros with improved output compared to ck_assert(). */
+/* OP may only be == or !=  */
+#define _ck_assert_ptr(X, OP, Y) do { \
+  void* _ck_x = (X); \
+  void* _ck_y = (Y); \
+  ck_assert_msg(_ck_x OP _ck_y, "Assertion '"#X#OP#Y"' failed: "#X"==%p, "#Y"==%p", _ck_x, _ck_y); \
+} while (0)
+#define ck_assert_ptr_eq(X, Y) _ck_assert_ptr(X, ==, Y)
+#define ck_assert_ptr_ne(X, Y) _ck_assert_ptr(X, !=, Y)
 
 void setup (void) {
 	return;
